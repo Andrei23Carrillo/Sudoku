57c59d99a3a671087d516895b6175447cfc0e2ea
Andrei23Carrillo <L163905602@chetumal.tecnm.mx>
2021-09-17 12:48:23 -0500

    commit

---
+++ Login-Sudoku/dificultad.js
@@ -0,0 +1,38 @@
+function Dificultad(){
+    window.location.href="dificultad.html";}
+
+function Estadisticas(){
+    window.location.href="estadisticas.html";
+}
+function Reglas(){
+    window.location.href="Reglas.html";}
+
+function Ingreso(){
+    window.location.href="ingreso.html";}
+
+function NivelFacil(){
+    window.location.href="NivelFacil.html";}
+
+function NivelMedio(){
+    window.location.href="NivelMedio.html";}
+
+function NivelDificil(){
+    window.location.href="NivelDificil.html";}
+
+function NivelExtremo(){
+    window.location.href="NivelExtremo.html";}
+function cambiarFondo1(){
+    if(document.getElementById('estilos').href.value="dificultad.css"){
+        document.getElementById('estilos').href = "dificultadVerde.css";
+    }
+}
+function cambiarFondo2(){
+    if(document.getElementById('estilos').href.value="dificultad.css"){
+        document.getElementById('estilos').href = 'dificultadRojo.css';
+    }
+}
+function cambiarFondo3(){
+    if(document.getElementById('estilos').href.value="dificultad.css"){
+        document.getElementById('estilos').href = 'dificultad.css';
+    }
+}
